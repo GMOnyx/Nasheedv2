8f07471aef210c7bf113b7a07a71772288e5b794
GMOnyx <abdarrahman2345@gmail.com>
2025-03-18 23:57:59 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-helmet-async'], // Add react-helmet-async here
-    },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
